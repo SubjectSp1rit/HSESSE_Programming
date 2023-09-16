cc2442a77afa98480a6777b967a6578b50f16046
SubjectSp1rit <dividedwestvnd@yandex.ru>
2023-09-16 19:28:59 +0300

    first task completed

---
+++ Seminars/Seminar03/Self/Task01/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    public static void Main()
+    {
+        Console.Write("Введите количество углов: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите длину стороны: ");
+        int l = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine((n * Math.Pow(l, 2)) / (4 * Math.Tan(Math.PI / n)));
+
+        Console.ReadKey();
+    }
+}
